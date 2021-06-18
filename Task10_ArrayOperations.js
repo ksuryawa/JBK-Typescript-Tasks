7007865092f7fdbf533597bf2f959016a218a3b6
ksuryawa <kapil.r.suryawanshi@gmail.com>
2021-06-18 16:30:24 +0100

    Update Task10

--- Task10_ArrayOperations.js
+++ Task10_ArrayOperations.js
@@ -1,7 +1,25 @@
-var a1 = [1, 2, 4, 5, 6, 8];
+var a1 = [10, 21, 4, 5, 6, 8];
 var a2 = [3, 4, 5, 6];
 var a3 = [];
-for (var i = 0; i < a1.length; i++) {
-    a3[i] = a1[i] + a2[i];
-}
-console.log(a3);
+var choice;
+var ArrayOps = function (choice) {
+    switch (choice) {
+        case '+':
+            console.log("-------- Addition --------");
+            for (var i = 0; i < a1.length; i++) {
+                a3[i] = a1[i] + a2[i];
+            }
+            break;
+        case '-':
+            console.log("-------- Subtraction --------");
+            for (var i = 0; i < a1.length; i++) {
+                a3[i] = a1[i] - a2[i];
+            }
+            break;
+        default:
+            console.log("Incorrect Operation...");
+            break;
+    }
+    console.log(a3);
+};
+ArrayOps("-");
--- Task10_ArrayOperations.ts
+++ Task10_ArrayOperations.ts
@@ -1,14 +1,33 @@
 
-var a1:number[]=[1,2,4,5,6,8];
+var a1:number[]=[10,21,4,5,6,8];
 var a2=[3,4,5,6];
 
 var a3=[];
+var choice:String;
 
-    for (let i = 0; i < a1.length; i++) {
-        a3[i]=a1[i]+a2[i];    
-    }
+var ArrayOps=(choice)=>{
+switch (choice) {
+    case '+':
+        console.log("-------- Addition --------")
+        for (let i = 0; i < a1.length; i++) {
+            a3[i]=a1[i]+a2[i];    
+        }
+        break;
 
-
-
+    case '-':
+        console.log("-------- Subtraction --------")
+        for (let i = 0; i < a1.length; i++) {
+            a3[i]=a1[i]-a2[i];    
+        }
+        break;    
+    default:
+        console.log("Incorrect Operation...")
+        break;
+}
+    
 
 console.log(a3);
+}
+
+
+ArrayOps("-");
